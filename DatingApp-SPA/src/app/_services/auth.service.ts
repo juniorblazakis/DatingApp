e7e5a1f7f5f8d679d2334a77561323f87d910e8b
Georges Blazakis <g.iblazakis@gmail.com>
2020-05-15 12:07:41 +1000

    add ngx-bootstrap and alertify

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseURL = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
 constructor(private http: HttpClient) { }
 
@@ -18,6 +21,7 @@ login(model: any){
         const user = response;
         if (user) {
           localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
         }
       })
     );
@@ -27,6 +31,10 @@ register(model: any){
   return this.http.post(this.baseURL + 'register', model);
 }
 
+loggedIn(){
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
+}
 
 
 }
